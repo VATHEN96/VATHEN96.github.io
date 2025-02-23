4272e1a53a5365319c1a36aef2310fc38bb07873
VATHEN96 <131343074+VATHEN96@users.noreply.github.com>
2025-02-23 19:24:07 +0700

    Fix

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,7 @@ declare global {
   interface Window {
     ethereum: {
       isMetaMask?: boolean;
+      isBraveWallet?: boolean;
       request: (request: { method: string; params?: Array<any> }) => Promise<any>;
       on: (eventName: string, callback: (...args: any[]) => void) => void;
       removeListener: (eventName: string, callback: (...args: any[]) => void) => void;
