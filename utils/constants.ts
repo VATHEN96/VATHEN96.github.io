e2e8aceaf66766034d852e96e0e3251b47ca0cfa
VATHEN96 <131343074+VATHEN96@users.noreply.github.com>
2025-02-23 19:19:18 +0700

    Fix

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,11 +1,11 @@
 import abi from "@/web3/artifacts/contracts/wowzarush.sol/wowzarush.json"
 
 export const contractAddress = "0x7144Da8697ec83F9f820460C6498DcA90fF20901";
-export const chainIdHex = "0x29"
-export const rpcUrl = "https://testnet.telos.net/evm"
-export const blockScannerUrl = "https://testnet.teloscan.io/"
+export const chainIdHex = "0x28"
+export const rpcUrl = "https://mainnet.telos.net/evm"
+export const blockScannerUrl = "https://teloscan.io/"
 export const currency = {
-    name: "Telos EVM Testnet",
+    name: "Telos EVM",
     symbol: "TLOS"
 }
 export const contractABI = abi.abi;
