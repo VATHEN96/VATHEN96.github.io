6fd325369a40e14bdb8276fc6bb9813cd98c9560
VATHEN96 <131343074+VATHEN96@users.noreply.github.com>
2025-02-23 17:24:40 +0700

    Fix

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,23 @@
 
 
 interface Window {
-    ethereum: {
+    ethereum?: {
       isMetaMask?: boolean;
       request: (args: { method: string; params?: any[] }) => Promise<any>;
       on?: (event: string, callback: (...args: any[]) => void) => void;
-      // Add other properties or methods from the Ethereum object that you plan to use
+      removeListener?: (event: string, callback: (...args: any[]) => void) => void;
     };
+  }
+
+  declare global {
+    interface Window {
+      ethereum?: {
+        isMetaMask?: boolean;
+        request: (args: { method: string; params?: any[] }) => Promise<any>;
+        on?: (event: string, callback: (...args: any[]) => void) => void;
+        removeListener?: (event: string, callback: (...args: any[]) => void) => void;
+      };
+    }
   }
   
 
